52af4139507972b0bbc2ac07f44a5fc229ed5c72
kostarask <kostaspaiania@gmail.com>
2021-10-31 20:59:02 +0200

    das

--- test2.php
+++ test2.php
@@ -1,20 +1,12 @@
-<?php
-include("includes/functions.php");
-require("includes/db.php");
-
-
-if(isset($_GET['message'])){
-  popMessage($_GET['message']);
-}
-
-$product = $_GET['message'];
-$products = $mysqli->query("SELECT product_id FROM products_final WHERE product_name LIKE '$product'");
- while ($row = $products->fetch_assoc()) {
-   $productID = $row['product_id'];
-   echo $productID;
- }
- 
-
-
-?>
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  
+</body>
+</html>
